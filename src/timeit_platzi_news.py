9581c0cdbff45ba909690b3242ea943cc4bab944
Ronaldmolinares <ronald.molinares01@uptc.edu.co>
2026-01-19 19:03:23 -0500

    solución mypy Actions

--- src/timeit_platzi_news.py
+++ src/timeit_platzi_news.py
@@ -32,11 +32,11 @@ def test_performance() -> None:
         articles = create_test_articles(size)
 
         time_original = timeit.timeit(
-            lambda articles=articles: find_duplicate_titles(articles),
+            lambda: bench_original(articles),
             number=1,
         )
         time_improved = timeit.timeit(
-            lambda articles=articles: find_duplicate_titles_improved(articles),
+            lambda: bench_improved(articles),
             number=1,
         )
 
